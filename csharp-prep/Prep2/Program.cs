4b9de1e2dab7cd149a8599497cb738a928316748
Bradley Cobia <cob20002@byui.edu>
2024-01-11 13:04:00 -0700

    Did the prep2 assignment. No Stretch Challenge.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,39 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string Answer = Console.ReadLine();
+        int Grade = int.Parse(Answer);
+
+        string letter = "";
+
+        if (Grade >= 90){
+            letter = "A";
+        }
+        else if(Grade >= 80 || Grade < 90){
+            letter = "B";
+        }
+        else if(Grade >= 70 || Grade < 80){
+            letter = "C";
+        }
+        else if (Grade >= 60 || Grade < 70){
+            letter = "D";
+        }
+        else{
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your Grade is: {letter}");
+
+        if (Grade >= 70){
+            Console.WriteLine("Congratulations, you passed!");
+        }
+        else{
+            Console.WriteLine("You have failed.");
+        }
     }
 }
