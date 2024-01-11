3e768d74a42c433958a2650df3b9b854d662c664
Bradley Cobia <cob20002@byui.edu>
2024-01-11 12:40:05 -0700

    Did the prep1 assignment

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string First_Name = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string Last_Name = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {Last_Name}, {First_Name} {Last_Name}.");
     }
 }
