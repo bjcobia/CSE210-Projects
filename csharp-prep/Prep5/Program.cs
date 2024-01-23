ef39c0aaee326775690081f3ab485ee03aa1ea1e
Bradley Cobia <cob20002@byui.edu>
2024-01-23 08:30:33 -0700

    Did prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,38 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+
+        SquareNumber(userNumber);
+
+
+
+       static void DisplayWelcome(){
+        Console.WriteLine("Welcome to the program!");
+       }
+
+       static string PromptUserName(){
+            Console.WriteLine("Please enter your name: ");
+            string userName = Console.ReadLine();
+            return userName;
+       }
+       static int PromptUserNumber(){
+            Console.WriteLine("Please enter your favorite number: ");
+            int userNumber = int.Parse(Console.ReadLine());
+            return userNumber;
+       }
+       static int SquareNumber(int userNumber){
+            int Squared = userNumber^2;
+            return Squared;
+       }
+
+
     }
 }
