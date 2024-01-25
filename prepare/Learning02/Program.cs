c19169056f1b18fc63b8162874b77706abe81ffc
Bradley Cobia <cob20002@byui.edu>
2024-01-24 22:12:07 -0700

    Finished Learning02

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Project Manager";
+        job1._company = "Northrup Gruman";
+        job1._startYear = 2015;
+        job1._endYear = 2020;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Electrical Engineer";
+        job2._company = "Boeing";
+        job2._startYear = 2021;
+        job2._endYear = 2023;
+
+        Resume resume1 = new Resume();
+        resume1._name = "Bradley Cobia";
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+
+        resume1.Display();
     }
 }
