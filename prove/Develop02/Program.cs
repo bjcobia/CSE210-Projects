c98876ae80287c3e989b8622a51fb845b74a673f
Bradley Cobia <cob20002@byui.edu>
2024-01-31 12:27:15 -0700

    started the first team project

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        string userChoice = "1";
+        while (userChoice != "5") {
+
+        Console.WriteLine("Welcome to the journal program!");
+        Console.WriteLine("Please select one of the following choices: ");
+        Console.WriteLine("1. Write\n2. Display\n3. Load\n4. Save\n5. Quit");
+
+        userChoice = Console.ReadLine();
+
+        switch (userChoice)
+        {
+            case "1":
+                
+                break;
+            case "2":
+
+                break;
+            case "3":
+
+                break;
+            case "4":
+
+                break;
+            case "5":
+
+                break;
+            default:
+                Console.WriteLine("Invalid choice. Please select a number between 1 and 5.");
+                break;
+        }
+        }
     }
 }
