7eb376a4dda19c44027413e642723e2e15fa5aee
Bradley Cobia <cob20002@byui.edu>
2024-02-03 18:25:03 -0700

    Finished with the prove assignment

--- prove/Develop02/PromptGenerator.cs
+++ prove/Develop02/PromptGenerator.cs
@@ -3,12 +3,13 @@ using System.IO;
 
 public class PromptGenerator{
         public List<string> _prompts = new List<string>{
-            "Do This",
-            "Do That",
-            "Do Stuff",
-            "Do Something",
-            "Do Something Else",
-            "Do Something Different"
+            "Who was the most interesting person I interacted with today?",
+            "What was the best part of my day?",
+            "How did I see the hand of the Lord in my life today?",
+            "What was the strongest emotion I felt today?",
+            "If I had one thing I could do over today, what would it be?",
+            "What was the best food or worst I ate today?",
+            "Was this a good or a bad day? Why?"
         };
     public string DisplayPrompt(){
         Random random = new Random();
