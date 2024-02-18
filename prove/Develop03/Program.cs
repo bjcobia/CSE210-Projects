eed39de2636344fb6040a40fc2bb8f346c6a08f2
Bradley Cobia <cob20002@byui.edu>
2024-02-17 23:35:50 -0700

    changes to Develop03

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,11 +1,12 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
         Reference reference = new Reference("2 Nephi", 4, 34);
-
+        
         Scripture scripture = new Scripture(reference, "O Lord, I have atrusted in thee, and I will btrust in thee forever. I will not put my ctrust in the arm of flesh; for I know that cursed is he that putteth his dtrust in the arm of flesh. Yea, cursed is he that putteth his trust in man or maketh flesh his arm.");
 
         scripture.GetRenderedText();
