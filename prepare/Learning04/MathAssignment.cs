5b720ab571396a76004e282316f5b8c62b328dbb
Bradley Cobia <cob20002@byui.edu>
2024-02-22 14:35:15 -0700

    Used Prettier extension

--- prepare/Learning04/MathAssignment.cs
+++ prepare/Learning04/MathAssignment.cs
@@ -4,10 +4,10 @@ public class MathAssignment : Assignment
     private string _problems;
 
     public MathAssignment(string studentName, string topic, string textbookSection, string problems) : base(studentName, topic)
-{
-    _textbookSection = textbookSection;
-    _problems = problems;
-}   
+    {
+        _textbookSection = textbookSection;
+        _problems = problems;
+    }
     public string GetHomeworkList()
     {
         string studentName = GetStudentName();
