4e6f550fcd9b30ba79025bcafdb6798a96868653
Bradley Cobia <cob20002@byui.edu>
2024-02-22 14:24:39 -0700

    Finished the Inheritance Learning Activity

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,16 @@
 using System;
+using System.Net.Security;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment = new Assignment("Brady Sanders", "C#");
+        MathAssignment mathAssignment = new MathAssignment("Johnathan Smith", "Math", "Section 7.3", "5-25");
+        WritingAssignment writingAssignment = new WritingAssignment("Mary Waters", "How to make better habits", "Atomic Habits");
+
+        Console.WriteLine(assignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+        Console.WriteLine(writingAssignment.GetWritingInfo());
     }
 }
